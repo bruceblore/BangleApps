7addc1e4a5979b876837cd4924562a4bc04983b9
Bruce Blore <bruceblore@protonmail.com>
2022-03-25 10:31:34 -0700

    Attempted to debug pomoplus crashing

--- apps/pomoplus/app.js
+++ apps/pomoplus/app.js
@@ -1,8 +1,7 @@
 Bangle.POMOPLUS_ACTIVE = true;  //Prevent the boot code from running. To avoid having to reload on every interaction, we'll control the vibrations from here when the user is in the app.
 
 const storage = require("Storage");
-const graphics = require("Graphics");
-const common = require("pomoplus-com.js")
+const common = require("pomoplus-com.js");
 
 //Expire the state if necessary
 if (
@@ -14,36 +13,37 @@ if (
 
 function drawButtons() {
   //Draw the backdrop
-  const BAR_TOP = graphics.getHeight() - 24;
-  graphics.setColor(0, 0, 1).setAlign(0, -1)
-    .fillRect(0, BAR_TOP, graphics.getWidth(), graphics.getHeight())
+  const BAR_TOP = g.getHeight() - 24;
+  g.setColor(0, 0, 1).setFontAlign(0, -1)
+    .fillRect(0, BAR_TOP, g.getWidth(), g.getHeight())
     .setColor(1, 1, 1);
 
   if (!common.state.wasRunning) {  //If the timer was never started, only show a play button
-    graphics.drawImage(common.BUTTON_ICONS.play, graphics.getWidth() / 2, BAR_TOP);
+    g.drawImage(common.BUTTON_ICONS.play, g.getWidth() / 2, BAR_TOP);
   } else {
-    graphics.drawLine(graphics.getWidth() / 2, BAR_TOP, graphics.getWidth() / 2, graphics.getHeight());
+    g.drawLine(g.getWidth() / 2, BAR_TOP, g.getWidth() / 2, g.getHeight());
     if (common.state.running) {
-      graphics.drawImage(common.BUTTON_ICONS.pause, graphics.getWidth() / 4, BAR_TOP)
-        .drawImage(common.BUTTON_ICONS.skip, graphics.getWidth() * 3 / 4, BAR_TOP);
+      g.drawImage(common.BUTTON_ICONS.pause, g.getWidth() / 4, BAR_TOP)
+        .drawImage(common.BUTTON_ICONS.skip, g.getWidth() * 3 / 4, BAR_TOP);
     } else {
-      graphics.drawImage(common.BUTTON_ICONS.reset, graphics.getWidth() / 4, BAR_TOP)
-        .drawImage(common.BUTTON_ICONS.play, graphics.getWidth() * 3 / 4, BAR_TOP);
+      g.drawImage(common.BUTTON_ICONS.reset, g.getWidth() / 4, BAR_TOP)
+        .drawImage(common.BUTTON_ICONS.play, g.getWidth() * 3 / 4, BAR_TOP);
     }
   }
 }
 
 function drawTimerAndMessage() {
+  let timeLeft;
   if (!common.state.wasRunning) {
-    var timeLeft = common.settings.workTime;
+    timeLeft = common.settings.workTime;
   } else if (common.state.running) {
-    var timeLeft = snapshot.nextChangeTime - (new Date()).getTime();
+    timeLeft = snapshot.nextChangeTime - (new Date()).getTime();
   } else {
-    var timeLeft = snapshot.nextChangeTime - common.state.pausedTime;
+    timeLeft = snapshot.nextChangeTime - common.state.pausedTime;
   }
 
-  graphics.setColor(0, 0, 0)
-    .setAlign(0, 0)
+  g.setColor(0, 0, 0)
+    .setFontAlign(0, 0)
     .setFont("Vector", 36)
 
     //Draw the timer
@@ -53,16 +53,16 @@ function drawTimerAndMessage() {
       let seconds = (timeLeft % 60000) / 1000;
 
       if (hours > 0) return `${parseInt(hours)}:${parseInt(minutes)}:${parseInt(seconds)}`;
-      else return `${parseInt(minutes)}:${parseInt(seconds)}`
-    })(), graphics.getWidth() / 2, graphics.getHeight() / 2)
+      else return `${parseInt(minutes)}:${parseInt(seconds)}`;
+    })(), g.getWidth() / 2, g.getHeight() / 2)
 
     //Draw the phase label
     .drawString(((currentPhase, numShortBreaks) => {
-      if (!common.state.wasRunning) return "Not started"
+      if (!common.state.wasRunning) return "Not started";
       else if (currentPhase == common.PHASE_LONG_BREAK) return "Long break!";
       else return `${currentPhase == common.PHASE_WORKING ? "Work" : "Short break"} ${numShortBreaks + 1}/${common.settings.numShortBreaks}`;
     })(snapshot.currentPhase, snapshot.numShortBreaks),
-      graphics.getWidth() / 2, graphics.getHeight() / 2 + 18);
+      g.getWidth() / 2, g.getHeight() / 2 + 18);
 
   //Perform vibrations and update the snapshot if needed
   if (timeLeft <= 0) {
@@ -75,7 +75,7 @@ drawButtons();
 Bangle.on("touch", (button, xy) => {
   //If we support full touch and we're not touching the keys, ignore.
   //If we don't support full touch, we can't tell so just assume we are.
-  if (xy !== undefined && xy.y <= graphics.getHeight() - 24) return;
+  if (xy !== undefined && xy.y <= g.getHeight() - 24) return;
 
   if (!common.state.wasRunning) {
     //If we were never running, there is only one button: the start button
@@ -133,7 +133,7 @@ function setupTimerInterval() {
     clearInterval(timerInterval);
   }
   setTimeout(() => {
-    timerInterval = setInterval(drawTimerAndMessage, 1000)
+    timerInterval = setInterval(drawTimerAndMessage, 1000);
     drawTimerAndMessage();
   }, 1000 - ((snapshot.nextChangeTimer - (new Date()).getTime()) % 1000));
 }
--- apps/pomoplus/common.js
+++ apps/pomoplus/common.js
@@ -1,8 +1,8 @@
 const storage = require("Storage");
 const heatshrink = require("heatshrink");
 
-exports.STATE_PATH = "pomoplus.state.json"
-exports.SETTINGS_PATH = "pomoplus.json"
+exports.STATE_PATH = "pomoplus.state.json";
+exports.SETTINGS_PATH = "pomoplus.json";
 
 exports.PHASE_WORKING = 0;
 exports.PHASE_SHORT_BREAK = 1;
@@ -13,20 +13,19 @@ exports.BUTTON_ICONS = {
     pause: heatshrink.decompress(atob("jEYwMA/4BBAX4CEA")),
     reset: heatshrink.decompress(atob("jEYwMA/4BB/+BAQPDAQPnAQIAKv///0///8j///EP//wAQQICBwQUCEhgyCHAQ+CIgI=")),
     skip: heatshrink.decompress(atob("jEYwMAwEIgHAhkA8EOgHwh8A/EPwH8h/A/0P8H/h/w/+P/H/5/8//v/3/AAoICBwQUCDQIgCEwQsCGQQ4CHwRECA"))
-}
+};
 
-let settings = storage.readJSON(SETTINGS_PATH);
-if (settings === undefined) {
-    settings = {
+exports.settings = storage.readJSON(SETTINGS_PATH);
+if (exports.settings === undefined) {
+    exports.settings = {
         workTime: 1500000,                  //Work for 25 minutes
         shortBreak: 300000,                 //5 minute short break
         longBreak: 900000,                  //15 minute long break
         numShortBreaks: 3,                  //3 short breaks for every long break
         pausedTimerExpireTime: 21600000,    //If the timer was left paused for >6 hours, reset it on next launch
         widget: false                       //If a widget is added in the future, whether the user wants it
-    }
+    };
 }
-exports.settings = settings;
 
 //Store the minimal amount of information to be able to reconstruct the state of the timer at any given time.
 //This is necessary because it is necessary to write to flash to let the timer run in the background, so minimizing the writes is necessary.
@@ -36,46 +35,50 @@ exports.STATE_DEFAULT = {
     startTime: 0,           //When the timer was last started. Difference between this and now is how long timer has run continuously.
     pausedTime: 0,          //When the timer was last paused. Used for expiration and displaying timer while paused.
     elapsedTime: 0          //How much time the timer had spent running before the current start time. Update on pause or user skipping stages.
+};
+exports.state = storage.readJSON(STATE_PATH);
+if (exports.state === undefined) {
+    exports.state = STATE_DEFAULT;
 }
-let state = storage.readJSON(STATE_PATH);
-if (state === undefined) {
-    state = STATE_DEFAULT
-}
-exports.state = state
 
 //Given a timer state, generate a snapshot, which contains all of the information one would normally expect in the state.
 exports.getSnapshot = function (state) {
     //Calculate how long the timer has been running overall
-    if (state.running) var totalRunningTime = (new Date()).getTime() - state.startTime + state.elapsedTime;
-    else var totalRunningTime = pausedTime - state.startTime + state.elapsedTime;
+    let totalRunningTime;
+    if (state.running) totalRunningTime = (new Date()).getTime() - state.startTime + state.elapsedTime;
+    else totalRunningTime = pausedTime - state.startTime + state.elapsedTime;
 
     //Calculate how long the timer has been running in the current cycle
+    let settings = exports.settings;
     let timeBeforeLongBreak = (settings.workTime + shortBreakTime) * settings.numShortBreaks;
     let fullCycleTime = timeBeforeLongBreak + settings.longBreak;
     let timeInCycle = totalRunningTime % fullCycleTime;
+    let nextChangeTime;
+    let currentPhase;
+    let numShortBreaks;
 
     if (timeInCycle < timeBeforeLongBreak) {
         //I have decided to call the work / short break cycles "subcycles", while the overall cycle involves some number of subcycles and a long break.
         let timeInSubCycle = timeInCycle % settings.numShortBreaks;
-        var currentPhase = timeInSubCycle < settings.workTime ? exports.PHASE_WORKING : exports.PHASE_SHORT_BREAK;
-        var numShortBreaks = timeInCycle / settings.numShortBreaks;
+        currentPhase = timeInSubCycle < settings.workTime ? exports.PHASE_WORKING : exports.PHASE_SHORT_BREAK;
+        numShortBreaks = timeInCycle / settings.numShortBreaks;
 
         //The change time is the amount of time left plus the current time.
-        if (currentPhase == exports.PHASE_WORKING) var nextChangeTime = (settings.workTime - timeInSubCycle) + (new Date()).getTime()
-        else var nextChangeTime = (settings.shortBreak - (timeInSubCycle - settings.workTime)) + (new Date()).getTime()
+        if (currentPhase == exports.PHASE_WORKING) nextChangeTime = (settings.workTime - timeInSubCycle) + (new Date()).getTime();
+        else nextChangeTime = (settings.shortBreak - (timeInSubCycle - settings.workTime)) + (new Date()).getTime();
 
     } else {
-        var nextChangeTime = (settimgs.longBreak - (timeInCycle - timeBeforeLongBreak)) + (new Date()).getTime();
-        var currentPhase = exports.PHASE_LONG_BREAK;
-        var numShortBreaks = settings.numShortBreaks;
+        nextChangeTime = (settimgs.longBreak - (timeInCycle - timeBeforeLongBreak)) + (new Date()).getTime();
+        currentPhase = exports.PHASE_LONG_BREAK;
+        numShortBreaks = settings.numShortBreaks;
     }
 
     return {
         nextChangeTime: nextChangeTime, //Epoch time of when the phase will change next
         currentPhase: currentPhase,     //What phase we are in now: work, short break, or long break
         numShortBreaks: numShortBreaks  //How many short breaks have we had since the last long break?
-    }
-}
+    };
+};
 
 exports.vibrate = function (snapshot) {
     //Vibrate for the next phase.
@@ -89,9 +92,4 @@ exports.vibrate = function (snapshot) {
             setTimeout(Bangle.buzz, 600);
         }
     } else Bangle.buzz(750, 1);
-}
-
-//Clean up after ourselves, as this code may be run on every boot and therefore has the potential to pollute the global scope for other apps.
-delete settings;
-delete state;
-delete storage;
+};
