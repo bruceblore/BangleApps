7ccd3422560f8f5e55c363b5711c83b3bdd0ba3e
Bruce Blore <bruceblore@protonmail.com>
2022-08-08 13:06:00 -0400

    Fixed rendering bugs in maintanance and the fact that it removes infoclk.json when uninstalled

--- apps/maintanance/app.js
+++ apps/maintanance/app.js
@@ -2,6 +2,17 @@
 
 const locale = require('locale')
 
+// Determine the font size needed to fit a string of the given length widthin maxWidth number of pixels, clamped between minSize and maxSize
+function getFontSize(length, maxWidth, minSize, maxSize) {
+    let size = Math.floor(maxWidth / length);  //Number of pixels of width available to character
+    size *= (20 / 12);  //Convert to height, assuming 20 pixels of height for every 12 of width
+
+    // Clamp to within range
+    if (size < minSize) return minSize;
+    else if (size > maxSize) return maxSize;
+    else return Math.floor(size);
+}
+
 // Display the GPS status
 function handleGPS(fix) {
     let text;
@@ -20,7 +31,7 @@ function handleGPS(fix) {
         else text = `Time set (${fix.satellites} sats)`;
     } else text = `GPS fix (${fix.satellites} sats)`;
 
-    g.reset().setFont('Vector', 24).setFontAlign(0, -1).drawString(text, 88, 24);
+    g.reset().clearRect(0, 24, g.getWidth(), 48).setFont('Vector', getFontSize(text.length, g.getWidth(), 6, 24)).setFontAlign(0, -1).drawString(text, 88, 24);
 }
 
 handleGPS(Bangle.getGPSFix());
@@ -28,7 +39,8 @@ Bangle.on('GPS', handleGPS);
 
 // Display the status line and full screen battery bar
 function statusAndBattBar() {
-    g.reset().setFont('Vector', 24).setFontAlign(-1, -1).drawString(`${E.getBattery()}% ${locale.time(new Date())}`, 0, 152);
+    let text = `${E.getBattery()}% ${locale.time(new Date())}`;
+    g.reset().clearRect(0, 48, g.getWidth(), g.getHeight()).setFont('Vector', getFontSize(text.length, g.getWidth(), 6, 24)).setFontAlign(-1, -1).drawString(text, 0, 152);
 
     if (E.getBattery() == 100) {
         g.setColor(0, 1, 0);
