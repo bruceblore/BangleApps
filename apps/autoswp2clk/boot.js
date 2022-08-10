31e48c009794790e8acdb40aa725c685f044004e
Bruce Blore <bruceblore@protonmail.com>
2022-08-09 20:35:29 -0400

    autoswp2clk actually works now

--- apps/autoswp2clk/boot.js
+++ apps/autoswp2clk/boot.js
@@ -14,43 +14,41 @@
  * Effectively, this allows applications to manage events as they usually do, but creates a "default" listener for swipe to load the clock that only gets called if the application never set a listener for swipe. From the perspective of the user, apps that use swipes can use their swipes, and apps that don't use swipes will have swipes go back to the clock.
  */
 
-const AUTOSWP2CLK = {
-  on: Bangle.on,                                  // Original, unmodified Bangle.on()
-  removeListener: Bangle.removeListener,          // Original Bangle.removeListener()
-  removeAllListeners: Bangle.removeAllListeners,  // Original Bangle.removeAllListeners()
-  swipeHandlers: []                               // Array of swipe handlers
-};
+Bangle.AUTOSWP2CLK_ORIG_on = Bangle.on;                                  // Original, unmodified Bangle.on()
+Bangle.AUTOSWP2CLK_ORIG_removeListener = Bangle.removeListener;          // Original Bangle.removeListener()
+Bangle.AUTOSWP2CLK_ORIG_removeAllListeners = Bangle.removeAllListeners;  // Original Bangle.removeAllListeners()
+let AUTOSWP2CLK_swipeHandlers = [];                                      // Array of swipe handlers
 
 Bangle.on = (eventName, func) => {
   if (eventName == 'swipe') {
-    AUTOSWP2CLK.swipeHandlers.push(func);
+    AUTOSWP2CLK_swipeHandlers.push(func);
   } else {
-    AUTOSWP2CLK.on(eventName, func);
+    Bangle.AUTOSWP2CLK_ORIG_on(eventName, func);
   }
 };
 
 Bangle.removeListener = (eventName, func) => {
   if (eventName == 'swipe') {
-    AUTOSWP2CLK.swipeHandlers.splice(AUTOSWP2CLK.swipeHandlers.indexOf(func), 1);
+    AUTOSWP2CLK_swipeHandlers.splice(AUTOSWP2CLK_swipeHandlers.indexOf(func), 1);
   } else {
-    AUTOSWP2CLK.removeListener(eventName, func);
+    Bangle.AUTOSWP2CLK_ORIG_removeListener(eventName, func);
   }
 };
 
 Bangle.removeAllListeners = (eventName) => {
   if (eventName == 'swipe') {
-    AUTOSWP2CLK.swipeHandlers = [];
+    AUTOSWP2CLK_swipeHandlers = [];
   } else {
-    AUTOSWP2CLK.removeAllListeners(eventName);
+    Bangle.AUTOSWP2CLK_ORIG_removeAllListeners(eventName);
   }
 };
 
 // Using the old Bangle.on rather than the new one
-AUTOSWP2CLK.on('swipe', (dirLR, dirUD) => {
-  if (AUTOSWP2CLK.swipeHandlers.length == 0) {
+Bangle.AUTOSWP2CLK_ORIG_on('swipe', (dirLR, dirUD) => {
+  if (AUTOSWP2CLK_swipeHandlers.length == 0) {
     load();
   } else {
-    for (let func of AUTOSWP2CLK.swipeHandlers) {
+    for (let func of AUTOSWP2CLK_swipeHandlers) {
       func(dirLR, dirUD);
     }
   }
